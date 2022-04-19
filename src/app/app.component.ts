c28b2ad89b73621dea42c4e0b265730c13342466
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-04-19 13:09:20 +0530

    setValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
- registrationForm = new FormGroup({
-userName : new FormControl(''),
-password  : new FormControl(''),
-confirmpassword : new FormControl(''),
+  registrationForm = new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+    confirmpassword: new FormControl(''),
 
-address: new FormGroup({
-  State: new FormControl(''),
-  City: new FormControl(''),
-  Pin: new FormControl(''),
-  
-})
-})
+    address: new FormGroup({
+      State: new FormControl(''),
+      City: new FormControl(''),
+      Pin: new FormControl(''),
+    }),
+  });
+
+  loadApiData(){
+    this.registrationForm.setValue({
+      userName: 'Harsh',
+      password:'12345',
+      confirmpassword: '12345',
+      address: {
+        State: 'something 123',
+        City: 'muzaffarnagar',
+        Pin:'251001'
+      }
+
+    })
+  }
 }
