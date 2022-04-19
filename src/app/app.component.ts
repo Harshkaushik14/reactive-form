b03b1bada3f5c3e73560b4d0d348eb2bc1539c73
Harshkaushik14 <kaushikharsh86@gmail.com>
2022-04-19 14:16:42 +0530

    formBuilder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  registrationForm = new FormGroup({
-    userName: new FormControl(''),
-    password: new FormControl(''),
-    confirmpassword: new FormControl(''),
 
-    address: new FormGroup({
-      State: new FormControl(''),
-      City: new FormControl(''),
-      Pin: new FormControl(''),
-    }),
-  });
+
+
+export class AppComponent {
+
+  registrationForm : FormGroup
+
+  constructor(private fb:FormBuilder){
+     this.registrationForm = this.fb.group({
+      userName: ['Someone'],
+      password:[''],
+      confirmpassword:[''],
+      address: this.fb.group({
+        Pin:[''],
+        City:[''],
+        State:['']
+
+      })
+})
+  }
+
+  // registrationForm = new FormGroup({
+  //   userName: new FormControl(''),
+  //   password: new FormControl(''),
+  //   confirmpassword: new FormControl(''),
+    // address: new FormGroup({
+    //   State: new FormControl(''),
+    //   City: new FormControl(''),
+    //   Pin: new FormControl(''),
+    // }),
+  // });
 
   loadApiData(){
     this.registrationForm.patchValue({
